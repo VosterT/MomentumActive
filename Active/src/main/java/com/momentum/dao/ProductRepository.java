30c3b1ecab5eea163386b0d1b08dd47bcbe05374
VosterT <46917987+VosterT@users.noreply.github.com>
2021-05-02 19:51:59 +0200

    Delete Active directory

--- Active/src/main/java/com/momentum/dao/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.momentum.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.momentum.model.Product;
-
-public interface ProductRepository extends JpaRepository<Product, Integer>{
-
-}
